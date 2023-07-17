77455c19d0836d5e6983e09f5a8bb2c16ca8dc13
athlol <82967841+athlol@users.noreply.github.com>
2023-07-18 00:55:17 +0545

    Update grade_calculator.py

--- grade_calculator.py
+++ grade_calculator.py
@@ -3,12 +3,12 @@ def calculate_average_grade():
     grades = []
 
     for i in range(num_subjects):
-        grade = float(input("Enter the grade for subject {}: ".format(i + 1)))
+        grade = float(input(f"Enter the grade for subject {i + 1}: "))
         grades.append(grade)
 
     total_grades = sum(grades)
     average_grade = total_grades / num_subjects
 
-    print("Average grade: {:.2f}".format(average_grade))
+    print(f"Average grade: {average_grade:.2f}")
 
 calculate_average_grade()
